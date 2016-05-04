72d93433b1f62e9fa72dd159d98b728c34878f6e
ihsoft <igor.zavoychinskiy@gmail.com>
2016-05-04 00:18:41 -0700

    Add original source with project
    - Total style refactoring.
    - Update() fixed to OnUpdate()

---
+++ Source/Properties/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+#region Using directives
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+#endregion
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle ("SurfaceLights")]
+[assembly: AssemblyDescription ("Surface Mounted Lights by Why485")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("")]
+[assembly: AssemblyProduct ("SurfaceLights")]
+[assembly: AssemblyCopyright ("Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International Public License")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
+// This sets the default COM visibility of types in the assembly to invisible.
+// If you need to expose a type to COM, use [ComVisible(true)] on that type.
+[assembly: ComVisible (false)]
+// The assembly version has following format :
+//
+// Major.Minor.Build.Revision
+//
+// You can specify all the values or you can use the default the Revision and 
+// Build Numbers by using the '*' as shown below:
+[assembly: AssemblyVersion ("1.2.3.1")]
+[assembly: AssemblyInformationalVersion ("Pre-release 1.2.3 Builf 1 for KSP v1.1")]
+[assembly: Guid ("a4d0ab40-75bd-44fb-80bc-f50c194faf4c")]
+[assembly: KSPAssembly ("SurfaceLights", 1, 2)]
+[assembly: AssemblyFlags (AssemblyNameFlags.PublicKey | AssemblyNameFlags.EnableJITcompileOptimizer)]
---
+++ Source/SurfaceLight.cs
@@ -0,0 +1,91 @@
+﻿// Surface Mounted Stock-Alike Lights for Self-Illumination
+// Author: Why485 (http://forum.kerbalspaceprogram.com/index.php?/profile/26795-why485/)
+// This software is distributed under
+// a Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International.
+
+using System;
+using System.Linq;
+using UnityEngine;
+
+namespace KSP_Light_Mods {
+
+public class SurfaceLight : ModuleLight {
+  [KSPField(isPersistant = false)]
+  public Vector3 startingRGB = new Vector3(1.0f, 0.9f, 0.8f);
+
+  Material mat;
+  Color materialColor;
+  Color colorLastFrame;
+
+  const float MIN_BRIGHTNESS = 0.5f;
+  protected bool loadedFromSavedCraft = false;
+
+  public override void OnAwake() {
+    base.OnAwake();
+    mat = GetComponentInChildren<MeshRenderer>().material;
+  }
+
+  public override void OnInitialize() {
+    base.OnInitialize();
+
+    // When this is loading in the editor, the lightX values come from the part
+    // config files. When this is loading in flight, they are saved from the editor.     
+    if (HighLogic.LoadedSceneIsEditor) {
+      InitEditor();
+    } else if (HighLogic.LoadedSceneIsFlight) {
+      InitFlight();
+    }
+  }
+
+  public override void OnLoad(ConfigNode node) {
+    loadedFromSavedCraft = true;
+  }
+
+  public override void OnUpdate() {     
+    // Setting material color is expensive. Only do this in the editor where light
+    // color can change, and only do it when the color has changed.
+    if (HighLogic.LoadedSceneIsEditor) {
+      materialColor = FilterColor(new Color(lightR, lightG, lightB));
+
+      // Update the color of the light texture so that it matches the light color.
+      if (materialColor != colorLastFrame) {
+        UpdateEditorColors();
+      }
+
+      colorLastFrame = materialColor;
+    }
+  }
+
+  protected virtual void UpdateEditorColors() {
+    //print("CUSTOM LIGHT: Updating Color");
+    mat.SetColor("_EmissiveColor", materialColor);
+  }
+
+  protected virtual void InitEditor() {
+    if (!loadedFromSavedCraft) {
+      lightR = startingRGB.x;
+      lightG = startingRGB.y;
+      lightB = startingRGB.z;
+    }
+
+    materialColor = FilterColor(new Color(lightR, lightG, lightB));
+    colorLastFrame = materialColor;
+
+    mat.SetColor("_EmissiveColor", materialColor);
+  }
+
+  protected virtual void InitFlight() {
+    mat.SetColor("_EmissiveColor", new Color(lightR, lightG, lightB));
+  }
+
+  // Using raw color looks kinda ugly, so do some minor filtering.
+  protected Color FilterColor(Color rawColor) {
+    rawColor.r = rawColor.r * (1.0f - MIN_BRIGHTNESS) + MIN_BRIGHTNESS;
+    rawColor.g = rawColor.g * (1.0f - MIN_BRIGHTNESS) + MIN_BRIGHTNESS;
+    rawColor.b = rawColor.b * (1.0f - MIN_BRIGHTNESS) + MIN_BRIGHTNESS;
+
+    return rawColor;
+  }
+}
+
+}  // namespace
