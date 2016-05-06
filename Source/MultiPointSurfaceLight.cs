a046c97afbea93495da9cff312d95b6bf34f6f2a
ihsoft <igor.zavoychinskiy@gmail.com>
2016-05-06 00:50:45 -0700

    Make own module for 4-way light
    - Use stock settings for RGB.
    - Handle broken animation: show OFF state when all lights are OFF, and
    ON otherwise.
    - Rename all GUI items to indicate the light number.

---
+++ Source/MultiPointSurfaceLight.cs
@@ -0,0 +1,137 @@
+﻿// Surface Mounted Stock-Alike Lights for Self-Illumination
+// Mod's author: Why485 (http://forum.kerbalspaceprogram.com/index.php?/profile/26795-why485/)
+// Module author: igor.zavoychinskiy@gmail.com
+// This software is distributed under
+// a Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International.
+
+using System;
+using UnityEngine;
+
+namespace KSP_Light_Mods {
+
+/// <summary>A module to control a light part that has multiple light sources that can be
+/// controlled independently.</summary>
+/// <remarks>
+/// Light name is used to make GUI strings so, keep it short and human readable.
+/// E.g. "L1", "L2", etc.
+/// </remarks>
+public class MultiPointSurfaceLight : ModuleLight {
+  /// <summary>Last known light state.</summary>
+  bool lastOnState = false;
+
+  /// <summary>All the light modules on the part.</summary>
+  ModuleLight[] allLightModules {
+    get {
+      if (_allLightModules == null) {
+        _allLightModules = part.GetComponents<ModuleLight>();
+      }
+      return _allLightModules;
+    }
+  }
+  ModuleLight[] _allLightModules;
+
+  /// <summary>Dimming animation effect.</summary>
+  /// <remarks>For now it's global, for all the lights.</remarks>
+  AnimationState animationState {
+    get {
+      if (!_animationState) {
+        FindAnimation();
+      }
+      return _animationState;
+    }
+  }
+  AnimationState _animationState;
+
+  /// <summary>Animation to dim the light(s).</summary>
+  /// <remarks>For now it's global, for all the lights.</remarks>
+  Animation animation {
+    get {
+      if (!_animation) {
+        FindAnimation();
+      }
+      return _animation;
+    }
+  }
+  Animation _animation;
+
+  /// <summary>Scans for the animation in the model and records the found values.</summary>
+  /// <remarks>Scanning model for components is an expensive operation so, cache the found values
+  /// assuming they won't change.
+  /// <para>In case of some control needs to mangle with animations in runtime allow this method to
+  /// be called by the children.</para>
+  /// </remarks>
+  protected void FindAnimation() {
+    var animations = part.FindModelComponents<Animation>();
+    foreach (var modelAnimation in animations) {
+      var modelAnimationState = modelAnimation[animationName];
+      if (modelAnimationState != null) {
+        _animation = modelAnimation;
+        _animationState = modelAnimationState;
+        _animationState.normalizedSpeed = 0;  // Freeze.
+        break;
+      }
+    }
+
+    // Animation is freezed so, it just applies the initial state.
+    if (_animation) {
+      animation.Play(animationName);
+    }
+  }
+
+  public override void OnLoad(ConfigNode node) {
+    base.OnLoad(node);
+    UpdateAnimationState();
+  }
+  
+  public override void OnStart(PartModule.StartState state) {
+    base.OnStart(state);
+    UpdateAnimationState();
+
+    // Rewrite GUI names to identify different lights. Only do it when part is created in the
+    // editor. Rewritten names will be stored in the save file so, no need to fix them every time.
+    // Though, editor starts part on every load so, soem check is needed to not modify names
+    // endlessly.
+    if (state == PartModule.StartState.Editor) {
+      foreach (var action in Actions) {
+        if (!action.guiName.StartsWith(lightName)) {
+          action.guiName = lightName + ": " + action.guiName;
+        }
+      }
+      foreach (var uiEvent in Events) {
+        if (!uiEvent.guiName.StartsWith(lightName)) {
+          uiEvent.guiName = lightName + ": " + uiEvent.guiName;
+        }
+      }
+      foreach (var field in Fields) {
+        var bf = field as BaseField;
+        if ((bf.name == "lightR" || bf.name == "lightG" || bf.name == "lightB")
+            && !bf.guiName.StartsWith(lightName)) {
+          bf.guiName = lightName + ": " + bf.guiName;
+        }
+      }
+    }
+  }
+
+  public void Update() {
+	  // Verify global state of the animtation on every light state change.
+	  // FIXME: It's not the best idea from the performance perspective. So, fix the animation!
+    if (lastOnState != isOn) {
+      UpdateAnimationState();
+    }
+  }
+
+	/// <summary>Verifies if all lights are OFF and sets animation to OFF.</summary>
+	/// <remarks>Animation shows light texture for all the lights at the same time. To make part
+  /// behaving nicely show the lighted texture if at least one of the lights is active. Disable the
+  /// texture if none is enabled.</remarks>
+	void UpdateAnimationState() {
+    lastOnState = isOn;
+    var allAreOff = true;
+    foreach (var module in allLightModules) {
+      allAreOff &= !module.isOn;
+    }
+    animationState.normalizedTime = allAreOff ? 0 : 1;
+  }
+}
+
+}  // namespace
