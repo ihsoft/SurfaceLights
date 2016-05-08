54cb260e71971b9e35978b737567048b31f736b5
ihsoft <igor.zavoychinskiy@gmail.com>
2016-05-08 01:52:37 -0700

    Pre-release 1.2.3.3

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -24,7 +24,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion ("1.2.3.2")]
+[assembly: AssemblyVersion ("1.2.3.3")]
 [assembly: AssemblyInformationalVersion ("Pre-release 1.2.3 Build 2 for KSP v1.1")]
 [assembly: Guid ("a4d0ab40-75bd-44fb-80bc-f50c194faf4c")]
 [assembly: KSPAssembly ("SurfaceLights", 1, 2)]
