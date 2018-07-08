14e77fe7a3c47b9a9c10619743749dc13a64536f
ihsoft <igor.zavoychinskiy@gmail.com>
2018-07-07 17:33:25 -0700

    Minor fix to the assembly description

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle ("SurfaceLights")]
-[assembly: AssemblyDescription ("Surface Mounted Lights by Why485")]
+[assembly: AssemblyDescription ("Surface Lights")]
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
 [assembly: AssemblyProduct ("SurfaceLights")]
