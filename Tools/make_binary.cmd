2f95f8c6005be3d0e99d57ed4612bdf2c1ded237
Igor Zavoychinskiy <igor.zavoychinskiy@gmail.com>
2020-12-26 18:30:46 -0800

    Update make_binary.cmd

--- Tools/make_binary.cmd
+++ Tools/make_binary.cmd
@@ -1,3 +1,3 @@
 @echo off
-REM SurfaceLights *requires* .NET compiler version 6.0 or higher and Framework version not less than 4.5.
-"C:\Program Files (x86)\MSBuild\14.0\Bin\MsBuild.exe" ..\Source\SurfaceLights.csproj /t:Clean,Build /p:Configuration=Release
+REM To get the latest MSBuild command line compiler go to: https://visualstudio.microsoft.com/thank-you-downloading-visual-studio/?sku=BuildTools&rel=16
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\amd64\MSBuild.exe" ..\Source\SurfaceLights.csproj /t:Clean,Build /p:Configuration=Release
